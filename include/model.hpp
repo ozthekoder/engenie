18994b211a1f8a1593331cd16122b46a260d629d
osman ozdemir <oozdemir2704@gmail.com>
2018-12-16 01:29:26 -0500

    build is broken

--- include/model.hpp
+++ include/model.hpp
@@ -17,16 +17,6 @@ using Json = nlohmann::json;
 namespace Engenie
 {
 
-static const std::string base64_chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz"
-    "0123456789+/";
-
-static inline bool is_base64(unsigned char c)
-{
-  return (isalnum(c) || (c == '+') || (c == '/'));
-}
-
 class Model : public Asset
 {
 
@@ -45,13 +35,7 @@ private:
   std::vector<Image> images;
   std::vector<Mesh> meshes;
 
-  void loadBuffers();
-  void loadImages();
   void parseNode(Json node);
-  bool isDataURI(const std::string &in);
-  void decodeDataURI(std::vector<unsigned char> &buf, std::string &mime_type, const std::string &in, size_t reqBytes);
-  std::string base64Decode(std::string const &encoded_string);
-  std::string base64Encode(unsigned char const *bytes_to_encode, unsigned int in_len);
 };
 
 }; // namespace Engenie
